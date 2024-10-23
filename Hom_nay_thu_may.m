61c400440cea8af0e6497a440a897ccc46aa6b3b
CodeMoiNgay <dangloctai00@gmail.com>
2024-10-23 16:26:10 +0700

    #Matlab

---
+++ Hom_nay_thu_may.m
@@ -0,0 +1,18 @@
+n = input("nhap n = ");
+switch n
+    case 2
+        hom_nay = "Thu 2"
+    case 3
+        hom_nay = "Thu 3"
+    case 4
+        hom_nay = "Thu 4"
+    case 5
+        hom_nay = "Thu 5"
+    case 6
+        hom_nay = "Thu 6"
+    case 7
+        hom_nay = "Thu 7"
+    otherwise
+        hom_nay = "Chu Nhat"
+end
+     
